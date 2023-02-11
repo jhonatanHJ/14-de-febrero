a9f92f7bccb785822a5500125b5e23a4bed87210
jhonatanHJ <125149385+jhonatanHJ@users.noreply.github.com>
2023-02-11 15:04:11 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const documentReady = () => {
+  const heroTitle = document.querySelector('.hero__title');
+  const heroButtonSi = document.getElementById('heroButtonSi');
+  const heroButtonNo = document.querySelector('#heroButtonNo');
+
+  const nowZomosNobios = () => {
+    alert('💞 Esta bien, juntos toda la vida!!💞');
+    alert('😱Nuestra Boda es Mañana😱');
+    location.href = 'https://www.youtube.com/watch?v=c5tH0YTxFnc&list=RDivKL-5zsEFI&index=3';
+  };
+
+  const evitarKCRompanMyBobo = () => {
+    heroButtonNo.style.position = 'absolute';
+    heroButtonNo.style.top = (Math.random() * window.innerHeight) + 'px';
+    heroButtonNo.style.left = (Math.random() * window.innerWidth) + 'px';
+  };
+
+
+  heroButtonSi.addEventListener('click', nowZomosNobios);
+  heroButtonNo.addEventListener('mouseover', evitarKCRompanMyBobo);
+};
+
+document.addEventListener('DOMContentLoaded', documentReady);
